4f80ac0b558354eedc890e7845cd33b888039f79
liaohongbin <805398283@qq.com>
2019-06-19 13:53:14 +0800

    test commit！

--- src/main/java/com/liao/DockerTestApplication.java
+++ src/main/java/com/liao/DockerTestApplication.java
@@ -3,6 +3,9 @@ package com.liao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * test commit
+ */
 @SpringBootApplication
 public class DockerTestApplication {
 
