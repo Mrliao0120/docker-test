934facb0d3a97ca27a9f3586d03d93163ed1c63b
liaohongbin <805398283@qq.com>
2019-06-20 10:09:44 +0800

    docker File  commit

--- src/main/java/com/liao/controller/HelloController.java
+++ src/main/java/com/liao/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.liao.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,4 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
+
+
+
+
+
+    @GetMapping(value = "/hello")
+    public String  hello(){
+        return "Ni 好Docker";
+    }
 }
